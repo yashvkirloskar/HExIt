ab824205197c3f1b1c328a511f48f1e7a6c5ccae
yashvkirloskar <yashvkirloskar@gmail.com>
2018-03-15 11:16:18 -0700

    Initial fix for hexagon issue.  Board is now hexagonal and guarantees a win condition.

--- Hex.py
+++ Hex.py
@@ -2,31 +2,29 @@
 # Should handle moves, and win conditions
 # No UI built in yet
 import util
+from GameStructures import *
+from HexBoard import *
 
 class Hex: 
     def __init__(self, game_size=5, player0='user', player1='user'):
         # Initialize game variables
-        self.game_size = 5
-        self.board = Board(self.game_size)
+        self.game_size = game_size
+        self.board = Board(game_size)
         self.player0 = player0
         self.player1 = player1
         self.turn = 0
         self.winner = None
         assert(self.player0 in ['user', 'ai'] and self.player1 in ['user', 'ai'])
 
-    
-
+    # Make a move
     def player_move(self, coord):
         if self.winner is not None:
             return None
         x, y = coord
 
         # Check that board is not being overwritten
-        assert(isinstance(self.board[x, y], EmptyPiece))
-        if self.turn == 0:
-            self.board[x, y] = Piece(self.turn, (x,y), self.board[x+1, y], self.board[x, y+1], self.board[x-1, y], self.board[x, y-1])
-        elif self.turn == 1:
-            self.board[x, y] = Piece(self.turn, (x,y), self.board[x+1, y], self.board[x, y+1], self.board[x-1, y], self.board[x, y-1])
+        assert(self.board[x, y].player == -1)
+        self.board[x, y].player = self.turn
 
         if self.check_win() == 'Win':
             print ("Game Over, Player " + str(self.turn) + " wins!")
@@ -61,75 +59,6 @@ class Hex:
                 if closed[node] == 0:
                     closed[node] += 1
                     for child in node.get_neighbors():
-                        if not isinstance(child, EmptyPiece):
-                            fifo.push(child)
+                        fifo.push(child)
 
-class EmptyPiece:
-    def __init__(self):
-        self.player = -1
-
-    def get_neighbors():
-        return []
-
-
-# Data structure to hold information about pieces
-# Useful for doing bfs
-class Piece:
-    def __init__(self, player=None, pos=None, neighbor1=None, neighbor2=None, neighbor3=None, neighbor4=None):
-        assert(player in [0, 1])
-        self.player = player
-        self.pos = pos
-        if neighbor1.player == self.player:
-            self.neighbor1 = neighbor1
-            self.neighbor1.neighbor3 = self
-        else:
-            self.neighbor1 = EmptyPiece()
-
-        if neighbor2.player == self.player:
-            self.neighbor2 = neighbor2
-            self.neighbor2.neighbor4 = self
-        else:
-            self.neighbor2 = EmptyPiece()
-
-        if neighbor3.player == self.player:
-            self.neighbor3 = neighbor3
-            self.neighbor3.neighbor1 = self
-        else:
-            self.neighbor3 = EmptyPiece()
-
-        if neighbor4.player == self.player:
-            self.neighbor4 = neighbor4
-            self.neighbor4.neighbor2 = self
-        else:
-            self.neighbor4 = EmptyPiece()
-
-    def get_neighbors(self):
-        return [self.neighbor1, self.neighbor2, self.neighbor3, self.neighbor4]
-
-# Board Util to deal with overflow
-class Board(list):
-    def __init__(self, size):
-        self.size = size
-        self.grid = [[EmptyPiece() for i in range(self.size)] for j in range(self.size)]
-
-    def __getitem__(self, tup):
-        key1, key2 = tup
-        if key1 >= self.size or key1 < 0 or key2 >= self.size or key2 < 0:
-            return EmptyPiece()
-        else:
-            return self.grid[key1][key2]
-
-    def __setitem__(self, tup, val):
-        key1, key2 = tup
-        if key1 >= self.size or key1 < 0 or key2 >= self.size or key2 < 0:
-            pass
-        else:
-            self.grid[key1][key2] = val
-
-    def __delitem__(self, key):
-        key1, key2 = tup
-        if key1 >= self.size or key1 < 0 or key2 >= self.size or key2 < 0:
-            pass
-        else:
-            self.grid[key1][key2] = EmptyPiece()
 
---
+++ HexBoard.py
@@ -0,0 +1,54 @@
+from GameStructures import *
+
+class Board(list):
+    # Initialize board and set all the neighbors so that each piece knows who it is next to
+    def __init__(self, game_size=5):
+        self.game_size=game_size
+        self.hexagons = [[Piece(pos=(i,j)) for i in range(self.game_size)] for j in range(self.game_size)]
+        self.hexagons[0][0].add_neighbor(self.hexagons[0][1], 2)
+        self.hexagons[0][0].add_neighbor(self.hexagons[1][0], 3)
+        for i in range(1, self.game_size):
+            self.hexagons[i][0].add_neighbor(self.hexagons[i-1][0], 6)
+            self.hexagons[0][i].add_neighbor(self.hexagons[0][i-1], 5)
+            self.hexagons[i][-1].add_neighbor(self.hexagons[i-1][-1], 6)
+            self.hexagons[-1][i].add_neighbor(self.hexagons[-1][i-1], 5)
+
+        for i in range(1, self.game_size-1):
+            for j in range(1, self.game_size-1):
+                self.hexagons[i][j].add_neighbor(self.hexagons[i-1][j+1], 1)
+                self.hexagons[i][j].add_neighbor(self.hexagons[i][j+1], 2)
+                self.hexagons[i][j].add_neighbor(self.hexagons[i+1][j], 3)
+                self.hexagons[i][j].add_neighbor(self.hexagons[i+1][j-1], 4)
+                self.hexagons[i][j].add_neighbor(self.hexagons[i][j-1], 5)
+                self.hexagons[i][j].add_neighbor(self.hexagons[i-1][j], 6)
+
+    # Override the list methods to allow for out of range calls
+    def __getitem__(self, tup):
+        key1, key2 = tup
+        if key1 >= self.game_size or key1 < 0 or key2 >= self.game_size or key2 < 0:
+            return EmptyPiece()
+        else:
+            return self.hexagons[key1][key2]
+
+    def __setitem__(self, tup, val):
+        key1, key2 = tup
+        if key1 >= self.game_size or key1 < 0 or key2 >= self.game_size or key2 < 0:
+            pass
+        else:
+            self.hexagons[key1][key2].player = val
+
+    def __delitem__(self, tup):
+        key1, key2 = tup
+        if key1 >= self.game_size or key1 < 0 or key2 >= self.game_size or key2 < 0:
+            pass
+        else:
+            self.hexagons[key1][key2].player = -1
+
+    def __str__(self):
+        board = [[0 for i in range(self.game_size)] for j in range(self.game_size)]
+        for i in range(self.game_size):
+            for j in range(self.game_size):
+                if self.hexagons[i][j].player != -1:
+                    board[i][j] = self.hexagons[i][j].player+1
+        return str(board)
+
