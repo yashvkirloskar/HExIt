a5748395dcc657d84c80525db834e11e541a340c
yashvkirloskar <yashvkirloskar@gmail.com>
2018-04-27 17:02:52 -0700

    Modifications to conv net

--- HexitConvNet.py
+++ HexitConvNet.py
@@ -9,7 +9,7 @@ class CNN:
         self.num_channels = input_shape[0]
         self.input_width = input_shape[1]
         self.input_height = input_shape[2]
-		
+
         self.batch_size = batch_size
         self.output_size = output_size
         self.conv_layer_depth = conv_layer_depth
@@ -21,9 +21,6 @@ class CNN:
         #name used for saving and restoring
         self.name = name
 
-
-
-	
     '''
     Building a convolutional net as described in the paper "Thinking Fast and Slow
     with Deep Learning and Tree Search"
@@ -50,47 +47,35 @@ class CNN:
     def buildGraph(self):
 
         # with tf.variable_scope("convnet_computationalgraph"):
-            #Setup placeholder for input
-        train_inputs = tf.placeholder(tf.float32, [2, self.batch_size, self.num_channels, self.input_width, self.input_height], name="inputs")
-        train_labels = tf.placeholder(tf.float32, [2, self.batch_size, self.output_size], name="labels")
-        mask = tf.placeholder(tf.float32, [2, self.batch_size, self.output_size], name="mask")
+        #Setup placeholder for input
+        train_inputs = tf.placeholder(tf.float32, [None, self.num_channels, self.input_width, self.input_height], name="inputs")
+        train_labels = tf.placeholder(tf.float32, [None, self.output_size], name="labels")
+        mask = tf.placeholder(tf.float32, [None, self.output_size], name="mask")
 
-        output = tf.reshape(train_inputs, [2*self.batch_size, self.num_channels, self.input_width, self.input_height])
         # Put tensor in correct shape (NHWC)
-        output = tf.transpose(output, [0, 2, 3, 1])
+        output = tf.transpose(train_inputs, [0, 2, 3, 1])
 
         #Set up convolutional layers
         for i in range(1, 14):
             output = self.buildConvolutionalOperation(output,self.conv_layer_depth, i)
 
         # Put inputs in terms of player 1 and player 2
-        output = tf.reshape(output, [2, self.batch_size, self.input_width-4, self.input_height-4, self.conv_layer_depth])
-		
-        outputWhite = tf.gather(output, 0, axis=0)
-        maskWhite = tf.gather(mask, 0, axis=0)
-        labelsWhite = tf.gather(train_labels, 0, axis=0)
-
-        outputBlack = tf.gather(output, 1, axis=0) 
-        maskBlack = tf.gather(mask, 0, axis=0)
-        labelsBlack = tf.gather(train_labels, 0, axis=0)
+        outputWhite, outputBlack = tf.split(output, 2, axis=0)
+        maskWhite, maskBlack = tf.split(mask, 2, axis=0)
+        labelsWhite, labelsBlack = tf.split(train_labels, 2, axis=0)
 
         # Using a single FC layer loss
         outputP1 = self.buildFullyConnectedLayerWithSoftmax(outputWhite, self.output_size, maskWhite)
         outputP2 = self.buildFullyConnectedLayerWithSoftmax(outputBlack, self.output_size, maskBlack)
 
         # #Loss
-        lossP1 = tf.scalar_mul(-1, tf.reduce_sum(tf.multiply(labelsWhite, outputP1), axis=1))
-        lossP2 = tf.scalar_mul(-1, tf.reduce_sum(tf.multiply(labelsBlack, outputP2), axis=1))
-		
-        output = tf.stack((outputP1, outputP2), axis=0)
-        output = tf.identity(output, name="output")
-        loss = tf.stack((lossP1, lossP2), axis=0)
-        loss = tf.identity(loss, name="loss")
+        lossP1 = tf.scalar_mul(-1, tf.reduce_mean(tf.reduce_sum(tf.multiply(labelsWhite, tf.log1p(outputP1)), axis=1)))
+        lossP2 = tf.scalar_mul(-1, tf.reduce_mean(tf.reduce_sum(tf.multiply(labelsBlack, tf.log1p(outputP2)), axis=1)))
+
+        output = tf.stack((outputP1, outputP2), axis=0, name="output")
+        loss = tf.stack((lossP1, lossP2), axis=0, name="loss")
 
         return train_inputs, train_labels, mask, output, loss
-
-
-
 
     '''
     Builds a convolutional operation to add to the computational graph based
@@ -114,47 +99,24 @@ class CNN:
     idx = the index of the layer 
     '''
     def buildFullyConnectedLayerWithSoftmax(self, fc_input, output_size, fc_mask):
-        input_shape = tf.shape(fc_input)
-        rest = input_shape[1] * input_shape[2] * input_shape[3]
-        fc_input = tf.reshape(fc_input, [self.batch_size, rest])
+        # input_shape = tf.shape(fc_input)
+        # 5x5x64
+        rest = (self.input_width-4) * (self.input_height-4) * self.conv_layer_depth
+        fc_input = tf.reshape(fc_input, [-1, rest])
+
         output = tf.layers.dense(fc_input, output_size)
         masked = tf.multiply(output, fc_mask)
         output = tf.nn.softmax(masked, axis=1)
+        temp = tf.multiply(output, fc_mask)
+        output = tf.divide(temp, tf.reduce_sum(tf.add(temp, 1e-12), axis=1, keepdims=True))
         return output
 
     def train(self, inputs, labels, mask):
 
 
         if(os.path.exists(self.name + "/convnet.meta")):
-
-            # # Add an op to initialize the variables.
-            # init_op = tf.global_variables_initializer()
-
-            # print("starting training from last checkpoint")
-
-            # with tf.Session() as session:
-            #     init_op.run()
-
-            #     #Restore the old graph and get relevant ops / variables
-            #     saver = tf.train.import_meta_graph(self.name + "/convnet.meta")
-            #     saver.restore(session,tf.train.latest_checkpoint(self.name))
-            #     graph = tf.get_default_graph()
-            #     input_op = graph.get_tensor_by_name("inputs:0")
-            #     predict_mask = graph.get_tensor_by_name("mask:0")
-
-            #     predict_labels = graph.get_tensor_by_name("labels:0")
-            #     feed_dict = {input_op:inputs, predict_mask:mask, predict_labels:labels}
-            #     op_to_restore = graph.get_tensor_by_name("output:0")
-            #     loss = graph.get_tensor_by_name("loss:0")
-
-            #     #Add an op to optimize the loss
-            #     #optimize_op = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
-            #     optimize_op = tf.get_collection('optimizer')[0]
-            #     _,loss_val, output = session.run([optimize_op, loss, op_to_restore], feed_dict=feed_dict)
-            #     total_loss = loss_val
-            #     save_path = saver.save(session, self.name +  "/" + "convnet")
-
-                    # Add an op to initialize the variables.
+            print ("Loading from previous checkpoint")
+            # Add an op to initialize the variables.
             init_op = tf.global_variables_initializer()
         
             with tf.Session() as session:
@@ -177,10 +139,13 @@ class CNN:
                 _,loss_val, output = session.run([optimize_op, loss_op, output_op], feed_dict=feed_dict)
                 total_loss = loss_val
                 save_path = saver.save(session, self.name +  "/" + "convnet")
-
-
-            # output = session.run([op_to_restore], feed_dict=feed_dict)
-
+                print ("===========Result Ops=============")
+                print (len(output))
+                print (output[0].shape)
+                print (output[0][0])
+                print ("===========Result Loss=============")
+                print (len(loss_val))
+                print (loss_val[0])
 
         else:
             print("no saved graph, building a new one")
@@ -212,7 +177,7 @@ class CNN:
                 print (result_ops[0][0])
                 print ("===========Result Loss=============")
                 print (len(result_loss))
-                print (result_loss[0][0])
+                print (result_loss[0])
 
             
 
@@ -220,7 +185,6 @@ class CNN:
 
     def predict(self, predict_input, predict_mask):
         # Add an op to initialize the variables.
-        # init_op = tf.global_variables_initializer()
     
         with tf.Session() as session:
 
@@ -232,10 +196,6 @@ class CNN:
             mask = graph.get_tensor_by_name("mask:0")
             feed_dict = {inputs: predict_input, mask: predict_mask}
             op_to_restore = graph.get_tensor_by_name("output:0")
-
-            # init_op.run()
-
-
             output = session.run([op_to_restore], feed_dict=feed_dict)
 
         return output
--- HexitConvNet_Test.py
+++ HexitConvNet_Test.py
@@ -11,23 +11,19 @@ batch_size = 256
 
 
 def convert_to_softmax(labels):
-	new_labels = np.copy(labels)
-	for i in range(len(labels)):
-		for j in range(len(labels[i])):
-			new_labels[i][j] = labels[i][j] / np.sum(labels[i][j])
-	return new_labels
+    return labels / np.sum(labels, axis=1, keepdims=True)
 
 
 
 
 ###########  Basic Sanity Check ###########
-rand_inputs = np.random.rand(2, batch_size, 6,9,9).astype(np.float32)
-rand_labels = convert_to_softmax(np.random.rand(2, batch_size, 25).astype(np.float32))
-rand_masks = np.ones((2, batch_size, 25)).astype(np.float32)
+rand_inputs = np.random.rand(2* batch_size, 6,9,9).astype(np.float32)
+rand_labels = convert_to_softmax(np.random.rand(2* batch_size, 25).astype(np.float32))
+rand_masks = np.ones((2* batch_size, 25)).astype(np.float32)
 convnet = HexitConvNet.CNN((6,9,9), 25, 64, batch_size, "test_dir1")
 convnet.train(rand_inputs, rand_labels, rand_masks)
 
-rand_inputs2 = np.random.rand(2, batch_size, 6,9,9).astype(np.float32)
+rand_inputs2 = np.random.rand(2* batch_size, 6,9,9).astype(np.float32)
 
 output1 = convnet.predict(rand_inputs2, rand_masks)
 output2 = convnet.predict(copy.deepcopy(rand_inputs2),rand_masks)
@@ -65,21 +61,21 @@ print(np.array_equal(output3, output2))
 print ("===============PRINT OUTPUT3=================")
 print (len(output3))
 print (output3[0].shape)
-print (output3)
+print (output3[0][0, 0])
 #####################################################################
 
 
 
 ###########   Testing to see if the CNN starts training from the original data ###########
-rand_inputs3 = np.random.rand(2, batch_size, 6,9,9).astype(np.float32)
-rand_labels3 = convert_to_softmax(np.random.rand(2, batch_size, 25).astype(np.float32))
+rand_inputs3 = np.random.rand(2* batch_size, 6,9,9).astype(np.float32)
+rand_labels3 = convert_to_softmax(np.random.rand(2* batch_size, 25).astype(np.float32))
 convnet.train(rand_inputs3, rand_labels3, rand_masks)
 
 output4 = convnet.predict(copy.deepcopy(rand_inputs2),rand_masks)
 print ("===========OUTPUT4=============")
 print (len(output4))
 print (output4[0].shape)
-print (output4)
+print (output4[0][0, 0])
 
 convnet3 = HexitConvNet.CNN((6,9,9), 25, 64, batch_size, "test_dir2")
 convnet3.train(rand_inputs3, rand_labels3, rand_masks)
@@ -93,11 +89,56 @@ output5 = np.array(output5)
 print(np.sum((output5-output4)**2))
 ##########################################################################################
 
+# Test with some zero masks
+rand_inputs4 = np.random.rand(2*batch_size, 6, 9, 9).astype(np.float32)
+rand_masks_with_zero1 = np.random.choice([0, 1], [2*batch_size, 25]).astype(np.float32)
+rand_labels4 = convert_to_softmax(np.random.rand(2* batch_size, 25).astype(np.float32)  * rand_masks_with_zero1)
+convnet3.train(rand_inputs4, rand_labels4, rand_masks_with_zero1)
 
+rand_inputs5 = np.random.rand(2*batch_size, 6, 9, 9).astype(np.float32)
+rand_masks_with_zero2 = np.random.choice([0, 1], [2*batch_size, 25]).astype(np.float32)
+output6 = convnet3.predict(rand_inputs5, rand_masks_with_zero2)
+print ("========OUTPUT6=========")
+print (len(output6))
+print (output6[0].shape)
+print (rand_masks_with_zero2[0])
+print (output6[0][0, 0])
+print (np.sum(output6[0][0, 0]))
+assert(np.count_nonzero(output6) == np.count_nonzero(rand_masks_with_zero2))
 
+# Test with some zero masks
+rand_inputs6 = np.random.rand(2*batch_size, 6, 9, 9).astype(np.float32)
+rand_masks_with_zero3 = np.random.choice([0, 1], [2*batch_size, 25]).astype(np.float32)
+rand_labels6 = convert_to_softmax(np.random.rand(2* batch_size, 25).astype(np.float32)  * rand_masks_with_zero1)
+convnet4 = HexitConvNet.CNN((6, 9, 9), 25, 64, batch_size, "test_dir3")
+convnet4.train(rand_inputs6, rand_labels6, rand_masks_with_zero3)
 
+rand_inputs7 = np.random.rand(2*batch_size, 6, 9, 9).astype(np.float32)
+rand_masks_with_zero4 = np.random.choice([0, 1], [2*batch_size, 25]).astype(np.float32)
+output7 = convnet4.predict(rand_inputs7, rand_masks_with_zero4)
+print ("========OUTPUT7=========")
+print (len(output7))
+print (output7[0].shape)
+print (rand_masks_with_zero4[0])
+print (output7[0][0, 0])
+print (np.sum(output7[0][0, 0]))
+assert(np.count_nonzero(output7) == np.count_nonzero(rand_masks_with_zero4))
+
+rand_input_8 = np.random.rand(2, 6, 9, 9).astype(np.float32)
+rand_input_8[1] = np.ones((6, 9, 9)).astype(np.float32)
+rand_mask_with_zero5 = np.random.choice([0, 1], [2, 25]).astype(np.float32)
+rand_mask_with_zero5[1] = np.zeros(25)
+output8 = convnet4.predict(rand_input_8, rand_mask_with_zero5)
+print ("==========OUTPUT8============")
+print (len(output8))
+print (output8[0].shape)
+print (rand_mask_with_zero5[0])
+print (rand_mask_with_zero5[1])
+print (output8[0][0])
+print (output8[0][1])
+assert(np.count_nonzero(output8) == np.count_nonzero(rand_mask_with_zero5))
 
 #Delete all directories at end
 shutil.rmtree("test_dir1")
 shutil.rmtree("test_dir2") 
-
+shutil.rmtree("test_dir3") 
