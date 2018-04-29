f28cc4d00b9daf0e6d41bd0a92577a7bd477966a
yashvkirloskar <yashvkirloskar@gmail.com>
2018-04-29 13:23:47 -0700

    Test for apprentice

--- Apprentice.py
+++ Apprentice.py
@@ -10,7 +10,7 @@ class Apprentice:
         self.name = name
         self.NN = HexitConvNet.CNN((6, board_size+4, board_size+4), board_size * board_size, 64,batch_size, name)
 
-    def train(self, states, actions, mask=None):
+    def train(self, states, actions):
         print("Entered Apprentice Train Function")
         mask = np.zeros((states.shape[0], self.board_size*self.board_size))
         for i, state in enumerate(states):
@@ -18,21 +18,21 @@ class Apprentice:
         self.NN.train(states, actions, mask)
         print("Exited Apprentice Train Functions")
 
-    def getActionDistribution(self, states, mask=None):
+    def getActionDistribution(self, states):
         mask = np.zeros((states.shape[0], self.board_size*self.board_size))
         for i, state in enumerate(states):
             mask[i] = (((state[0, 2:-2, 2:-2] + state[1, 2:-2, 2:-2])-1)*-1).flatten()
         return self.NN.predict(states, mask)
 
-    def predict(self, states, mask=None):
+    def predict(self, states):
         print("Entered Apprentice Predict Function")
-        return self.getActionDistribution(states, mask)
+        return self.getActionDistribution(states)
         print("Exited Apprentice Predict Function")
 
     # Takes in a state_to_channel of size [1, 6, 9, 9] and outputs a [25,] action distribution 
-    def getActionDistributionSingle(self, state, turn, mask=None):
+    def getActionDistributionSingle(self, state, turn):
         stateInput = np.zeros((2, 6, self.board_size+4, self.board_size+4))
         stateInput[0 if turn == 1 else 1] = state
         mask = np.zeros((2, self.board_size*self.board_size))
         mask[0 if turn == 1 else 1] = (((state[0, 2:-2, 2:-2] + state[1, 2:-2, 2:-2])-1)*-1).flatten()
-        return self.NN.predict(stateInput, mask)[0 if turn == 1 else 1]
+        return self.NN.predict(stateInput, mask)[0][0 if turn == 1 else 1]
--- Test_Apprentice.py
+++ Test_Apprentice.py
@@ -1,5 +1,60 @@
 from Apprentice import *
-from MCTS
+from MCTS import *
+import shutil
 
-def testInit():
-    apprentice = Apprentice()
+def testTrain():
+    print ("Starting testTrain")
+    apprentice = Apprentice("testApprentice", 5, 2)
+    mcts = MCTS(5, 2, 50, 3)
+    batch, labels = mcts.generateExpertBatch()
+    apprentice.train(batch, labels)
+    shutil.rmtree("testApprentice")
+    print ("Training occurred without incident, so test passed")
+
+def testGetActionDistribution():
+    print ("Starting testGetActionDistribution")
+    apprentice = Apprentice("testApprentice", 5, 2)
+    mcts = MCTS(5, 2, 50, 3)
+    batch, labels = mcts.generateExpertBatch()
+    apprentice.train(batch, labels)
+    predicted = apprentice.predict(batch)
+    print ("Printing out correct action distribution")
+    print (np.sum(labels[0]))
+    print (labels[0])
+    print ("Printing out predicted action distribution")
+    print (predicted[0])
+    shutil.rmtree("testApprentice")
+    print ("Look at outputs to judge whether passed")
+
+def testGetActionDistributionSingle():
+    print ("Starting testGetActionDistributionSingle")
+    apprentice = Apprentice("testApprentice", 5, 2)
+    mcts = MCTS(5, 1, 50, 3)
+    batch, labels = mcts.generateExpertBatch()
+    apprentice.train(batch, labels)
+    print ("Predicting for player 1")
+    predicted = apprentice.getActionDistributionSingle(batch[0], 1)
+    print ("Printing out correct action distribution")
+    print (labels[0])
+    print (np.sum(labels[0]))
+    print ("Printing out predicted action distribution")
+    print (predicted[0])
+    print ("Predicting for player 2")
+    predicted = apprentice.getActionDistributionSingle(batch[1], -1)
+    print ("Printing out correct action distribution")
+    print (np.sum(labels[1]))
+    print (labels[1])
+    print ("Printing out predicted action distribution")
+    print (predicted[0])
+    shutil.rmtree("testApprentice")
+    print ("Single actionDistibution passed")
+
+def main():
+    print ("Staring Apprentice Tests")
+    testTrain()
+    testGetActionDistribution()
+    testGetActionDistributionSingle()
+    print ("All tests passed")
+
+if __name__ == '__main__':
+    main()
