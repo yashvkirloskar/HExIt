19f7cb1af73d0f0cfc89a9a59a067088a0e19daf
Aravind Sivakumar <aravind.sivakumar@berkeley.edu>
2018-04-28 19:29:45 -0700

    added integration test

---
+++ Test_Integration.py
@@ -0,0 +1,25 @@
+import numpy as np
+from MCTS import MCTS
+from Apprentice import Apprentice
+
+
+
+# Create data batch, feed it to apprentice, train, then predict, make sure shapes are correct
+def testBasicIntegration():
+    batch_size = 4
+    mcts = MCTS(size=5, batch_size=batch_size, simulations_per_state=500, max_depth=4, apprentice=None)
+    train_inputs, train_labels = mcts.generateExpertBatch()
+
+    apprentice = Apprentice(name="test_basic_integration", board_size=5, batch_size=batch_size)
+    # not sure what mask does
+    apprentice.train(train_inputs, train_labels) # broken
+
+    # once train works, add a line to call apprentice.predict, and examine output shape.
+
+def main():
+    print ("Testing Integration...")
+    testBasicIntegration()
+    print ("All tests passed!")
+
+if __name__ == '__main__':
+    main()
